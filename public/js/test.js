7b32f6f1ded329b45e2860bb5ce0c06f0112cc4d
Javier Iregui <iregui.javi@gmail.com>
2023-08-22 04:22:19 +0200

    fixed

--- public/js/test.js
+++ public/js/test.js
@@ -1,12 +1,11 @@
 const socket = io();
 
 socket.on('head?', () => {
-    console.log("kk")
     socket.emit('head', window.isHead)
 });
 
 socket.on('redirect', (destination) => {
-    //window.location.href = destination;
+    window.location.href = destination;
 });
 
 socket.on('offer?', () => {
