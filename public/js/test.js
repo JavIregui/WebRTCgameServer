2a9445e869ec7e7786613602c621d8157db2f9a0
Javier Iregui <iregui.javi@gmail.com>
2023-08-22 02:55:52 +0200

    test

--- public/js/test.js
+++ public/js/test.js
@@ -1,4 +1,4 @@
-const socket = io('https://webrtcgameserver-production.up.railway.app:1903', { transports: ["websocket", 'polling', 'flashsocket'] });
+const socket = io('https://webrtcgameserver-production.up.railway.app:1903', { enabledTransports: ['ws', 'wss'] });
 
 socket.on('head?', () => {
     console.log("kk")
